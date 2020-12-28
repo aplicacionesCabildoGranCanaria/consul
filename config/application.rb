79b8ad57cec7c39803e3a309d18a6919f1b14bc2
pablogg <pablo.garcia@enreda.coop>
2020-12-28 15:01:56 +0100

    Error en deploy

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-#Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)
 
 module Consul
   class Application < Rails::Application
